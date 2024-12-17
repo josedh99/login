b19c3770ea63f6d06816e8d516461bfab8e194de
jose <jamd@itpfp.com>
2024-12-17 13:39:01 +0100

    segundoCommit

---
+++ WindowsFormsApp1/Data/Conection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+using Google.Protobuf;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1.Data
+{
+    internal class Conection
+    {
+        public static MySqlConnection connection;
+
+        public static String server = "127.0.0.1";
+        public static String dataBase = "login";
+        public static String user = "root";
+        public static String contrasena = "1234";
+        public static void OpenConection()
+        {
+            string connectionString = $"server={server};database={dataBase};user={user};password={contrasena};";
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
+            if (connection.State == ConnectionState.Open)
+            {
+                MessageBox.Show("conexion establecida");
+            } else
+            {
+                MessageBox.Show("Eo: conexion no establecida");
+            }
+            connection.Close();
+        }
+
+        public static void ClousConection()
+        {
+
+        }
+    }
+}
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -15,6 +15,7 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+            Data.Conection.OpenConection();
         }
 
         private void button1_Click(object sender, EventArgs e)
